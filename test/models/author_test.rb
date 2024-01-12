17c97b0830682f7c38e1823ffff4fce9a7d0e655
LeonardoSordi <156307616+LeonardoSordi@users.noreply.github.com>
2024-01-12 11:52:33 +0100

    Add files via upload

--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
+require 'minitest/autorun'
 
 class AuthorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def test_author_articles_count
+
+    author = Author.create(name: "Test", surname: "Author", age: 40)
+    assert author.articles_count == 0
+    puts(author.inspect)
+    article = Article.create(title: "testTitle", body: "test body for article", author_id: author.id, status: "public" )
+    puts(article.inspect)
+    assert author.articles_count == 1
+    author.destroy
+    article.destroy
+
+  end
+
 end
