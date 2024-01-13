b76f0d159f7992abdcb5c7d4d4fce326b9d6cf3b
me-sparo <115223267+me-sparo@users.noreply.github.com>
2024-01-13 18:36:22 +0100

    revisione authors_controller_test.rb
    Tutti i test nel file passano tranne il test per la chiamata GET a /authors/:id/edit

--- test/controllers/authors_controller_test.rb
+++ test/controllers/authors_controller_test.rb
@@ -3,6 +3,9 @@ require "test_helper"
 class AuthorsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @author = Author.create(name: "Test", surname: "Author", age: 40)
+    @integer = 3
+    puts @integer
+    @integer += 1
   end
 
   test "should get index" do
@@ -23,21 +26,35 @@ class AuthorsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+
+  # Non esistono i file edit.json.jbuilder e _form.json.jbuilder che creano il file JSON per la risposta alla chiamata
+  # Di conseguenza il test fallisce se si imposta as: :json come risposta
+  # Inoltre la funzione edit del controller è vuota
+  # Cancellare il test?
   test "should get edit" do
     get edit_author_url(@author), as: :json
+
     assert_response :success
   end
 
+
   test "should update author" do
-    patch author_url(@author), params: { author: {  } }
-    assert_redirected_to author_url(@author)
+
+    patch author_url(@author), params: { author: { age: 36  } }, as: :json
+
+    @author.reload
+
+    puts @author.inspect
+
+    assert_response :success
+    assert @author.age == 36
   end
 
   test "should destroy author" do
     assert_difference("Author.count", -1) do
-      delete author_url(@author)
+      delete author_url(@author), as: :json
     end
 
-    assert_redirected_to authors_url
+    assert :success
   end
 end
