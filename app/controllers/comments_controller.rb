ffae63d745e5d8b2769b62b779aa32c273373639
Leonardo-Sordi <115223267+me-sparo@users.noreply.github.com>
2024-01-18 19:15:01 +0100

    article creator cannot comment owned articles

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,43 @@
 class CommentsController < ApplicationController
+
+  before_action :set_article_from_params, only: [:create]
+  before_action :set_article_creator, only: [:create]
+  before_action :forbid_article_owner, only: [:create]
     def create
-      @article = Article.find(params[:article_id])
-      @comment = @article.comments.create(comment_params)
-      redirect_to article_path(@article)
+      @article.comments.create(comment_params)
+      render json: {comment: @article.comments.last, status: :success}
     end
 
     def destroy
-      @article = Article.find(params[:article_id])
-      @comment = @article.comments.find(params[:id])
-      @comment.destroy
-      redirect_to article_path(@article), status: :see_other
     end
 
-    private
-      def comment_params
-        params.require(:comment).permit(:commenter, :body, :status)
-      end
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :status, :article_id)
   end
+  def get_comment
+    @comment = Comment.find(params[:comment_id])
+  end
+  def set_article_from_comment
+    @article = Article.find_by(id: @comment.article_id)
+  end
+  def set_article_from_params
+    @article = Article.find_by(id: comment_params[:article_id])
+  end
+  def set_article_creator
+    @article_creator = comment_params[:commenter]
+  end
+  def set_commenter
+    if Author.find_by(key: params[:author_key]).present?
+      @commenter = Author.find_by(key: params[:author_key])
+    else
+      render json: {}, status: :bad_request
+    end
+  end
+  def forbid_article_owner
+    if @commenter == @article_creator
+      render json: {}, status: :forbidden
+    end
+  end
+
+end
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @article_creator = Author.create(name: "Martin", surname: "with key")
+    @commenter = Author.create(name: "andrea", surname: "with key")
+    @article = Article.create(title: "author has key", body: "standard article body", status: "public", author_id: @article_creator.id)
+
+  end
+
+  test "article creator cannot comment its article" do
+    post article_comments_url(@article), params: {comment: {commenter: @article_creator, body: "comment body.", status: "public", article_id: @article.id }}, as: :json
+    assert_response :forbidden
+  end
+
 end
