a61b377fc4af4bb5ce12a976c543c09d761c5ea1
Leonardo-Sordi <115223267+me-sparo@users.noreply.github.com>
2024-01-19 19:06:40 +0100

    comment destroy update
    only comment creator and article author can destroy comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,36 @@
 class CommentsController < ApplicationController
 
-
+  before_action :get_comment, only: [:destroy, :update]
   before_action :set_article_from_params, only: [:create]
+  before_action :set_article_from_comment, only: [:destroy]
+
   before_action :set_commenter, only: [:create, :update]
-  before_action :set_article_creator, only: [:create]
+  before_action :set_article_creator, only: [:create, :destroy]
+
   before_action :forbid_creation_article_owner, only: [:create]
+  before_action :forbid_destroy_if_not_owner, only: [:destroy]
   def create
     @article.comments.create(comment_params)
     render json: {comment: @article.comments.last, status: :success}
   end
 
   def destroy
+    if @comment.destroy!
+    render json: {}, status: :no_content
+    end
+
   end
 
   def update
   end
+
 
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body, :status, :article_id)
   end
   def get_comment
-    @comment = Comment.find(params[:comment_id])
+    @comment = Comment.find(params[:id])
   end
 
   def set_article_from_comment
@@ -45,6 +54,12 @@ class CommentsController < ApplicationController
   end
   def forbid_creation_article_owner
     if comment_params[:commenter] == @article_creator.key
+      render json: {}, status: :forbidden
+    end
+  end
+
+  def forbid_destroy_if_not_owner
+    unless params[:author_key]==@article.author.key || params[:author_key]==@comment.commenter
       render json: {}, status: :forbidden
     end
   end
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -4,8 +4,10 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
 
   setup do
     @article_creator = Author.create(name: "Martin", surname: "with key")
-    @commenter = Author.create(name: "Andrea", surname: "with key")
+    @comment_creator = Author.create(name: "Andrea", surname: "with key")
+    @random_author = Author.create(name: "7", surname: "with key")
     @article = Article.create(title: "author has key", body: "standard article body", status: "public", author_id: @article_creator.id)
+    @comment = Comment.create(commenter: @comment_creator.key, body: "testo commento", status: "public", article_id: @article.id)
   end
 
   test "article creator cannot comment its article" do
@@ -14,8 +16,25 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "authors can comment articles" do
-    post article_comments_url(@article), params: {comment: {commenter: @commenter.key, body: "comment body.", status: "public", article_id: @article.id }}, as: :json
+    post article_comments_url(@article), params: {comment: {commenter: @comment_creator.key, body: "comment body.", status: "public", article_id: @article.id }}, as: :json
     assert_response :success
   end
+
+  test "comment creator can destroy comment" do
+    delete article_comment_url(@article, @comment), params: {author_key: @comment_creator.key}
+    assert_response :no_content
+  end
+
+
+  test "article creator can destroy comment" do
+    delete article_comment_url(@article, @comment), params: {author_key: @article_creator.key}
+    assert_response :no_content
+  end
+
+  test "random authors cannot destroy comment" do
+    delete article_comment_url(@article, @comment), params: {author_key: @random_author.key}
+    assert_response :forbidden
+  end
+
 
 end
