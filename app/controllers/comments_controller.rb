c9daf2a784cdea8079d1e46b35180a9a7d5051de
Leonardo-Sordi <115223267+me-sparo@users.noreply.github.com>
2024-01-19 02:38:24 +0100

    Commenter author check update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,20 @@
 class CommentsController < ApplicationController
 
-  before_action :set_article_from_params, only: [:create]
-  before_action :set_article_creator, only: [:create]
-  before_action :forbid_article_owner, only: [:create]
-    def create
-      @article.comments.create(comment_params)
-      render json: {comment: @article.comments.last, status: :success}
-    end
 
-    def destroy
-    end
+  before_action :set_article_from_params, only: [:create]
+  before_action :set_commenter, only: [:create, :update]
+  before_action :set_article_creator, only: [:create]
+  before_action :forbid_creation_article_owner, only: [:create]
+  def create
+    @article.comments.create(comment_params)
+    render json: {comment: @article.comments.last, status: :success}
+  end
+
+  def destroy
+  end
+
+  def update
+  end
 
   private
   def comment_params
@@ -18,24 +23,28 @@ class CommentsController < ApplicationController
   def get_comment
     @comment = Comment.find(params[:comment_id])
   end
+
   def set_article_from_comment
     @article = Article.find_by(id: @comment.article_id)
   end
+
   def set_article_from_params
     @article = Article.find_by(id: comment_params[:article_id])
   end
+
   def set_article_creator
-    @article_creator = comment_params[:commenter]
+    @article_creator = @article.author
   end
+
   def set_commenter
-    if Author.find_by(key: params[:author_key]).present?
-      @commenter = Author.find_by(key: params[:author_key])
+    if comment_params[:commenter].present?
+    @commenter = Author.find_by(key:comment_params[:commenter])
     else
       render json: {}, status: :bad_request
     end
   end
-  def forbid_article_owner
-    if @commenter == @article_creator
+  def forbid_creation_article_owner
+    if comment_params[:commenter] == @article_creator.key
       render json: {}, status: :forbidden
     end
   end
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -4,19 +4,18 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
 
   setup do
     @article_creator = Author.create(name: "Martin", surname: "with key")
-    @commenter = Author.create(name: "andrea", surname: "with key")
+    @commenter = Author.create(name: "Andrea", surname: "with key")
     @article = Article.create(title: "author has key", body: "standard article body", status: "public", author_id: @article_creator.id)
-
   end
 
   test "article creator cannot comment its article" do
-    post article_comments_url(@article), params: {comment: {commenter: @article_creator, body: "comment body.", status: "public", article_id: @article.id }}, as: :json
+    post article_comments_url(@article), params: {comment: {commenter: @article_creator.key, body: "comment body.", status: "public", article_id: @article.id }}, as: :json
     assert_response :forbidden
   end
 
   test "authors can comment articles" do
-    post article_comments_url(@article), params: {comment: {commenter: @commenter, body: "comment body.", status: "public", article_id: @article.id }}, as: :json
-    assert_response :forbidden
+    post article_comments_url(@article), params: {comment: {commenter: @commenter.key, body: "comment body.", status: "public", article_id: @article.id }}, as: :json
+    assert_response :success
   end
 
 end
