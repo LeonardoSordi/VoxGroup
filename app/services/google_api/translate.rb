e3640815e3b4aaaaa81f13b008f82640a9a5d1d6
Leonardo-Sordi <115223267+me-sparo@users.noreply.github.com>
2024-01-30 19:16:12 +0100

    google client handling update

--- app/services/google_api/translate.rb
+++ app/services/google_api/translate.rb
@@ -3,30 +3,38 @@ require "google/cloud/translate/v2"
 module GoogleApi
   class Translate
 
-    attr_reader :errors
+    attr_accessor :errors
+    attr_reader :client
 
     def initialize
-      @errors=[]
-      super
+
+    @errors=[]
+    @client = client_setup
+
     end
 
-    ENV["GOOGLE_APPLICATION_CREDENTIALS"] = "config/credenziali-Google-Translate-api.json"
+    def client_setup
+      ENV["GOOGLE_APPLICATION_CREDENTIALS"] = "config/credenziali-Google-Translate-api.json"
 
-    #attempts calling client 10 times before returning error
-    (1..10).each { |i|
-      @client = Google::Cloud::Translate::V2.new
-      break if @client != nil
-    }
+      #attempts calling client 10 times before returning error
+      client_init = Google::Cloud::Translate::V2.new
+      (1..10).each { |i|
+        break if client_init != nil
+        client_init = Google::Cloud::Translate::V2.new
+      }
 
-    puts @client.inspect
-
-    def self.call_translation(from_text, from_language, to_language)
-      if @client==nil
+      if client_init==nil
         @errors.push("Failed to establish connection with Google client")
         false
+      else
+        client_init
       end
-      @client.translate from_text, to: to_language, from: from_language
     end
 
+    def call_translation(from_text, from_language, to_language)
+      unless @client==nil
+        @client.translate from_text, to: to_language, from: from_language
+      end
+    end
   end
 end
--- app/services/translate_article.rb
+++ app/services/translate_article.rb
@@ -17,20 +17,25 @@ class TranslateArticle
     if @from_language == @to_language || @article.language == @to_language
       @errors.push( "Bad language selection" )
     else
-      translated_text = self.translate_service
+      translated_text = self.translate_article
+      puts translated_text.inspect
         unless translated_text==false
         @article.body=translated_text
         @article.language=@to_language
         @article.save
         end
-      end
+    end
+    puts @errors
     return self
   end
 
-  def translate_service
-    service = GoogleApi::Translate.call_translation(@article.body, @from_language, @to_language)
-    if service==false
-      @errors += service.errors
+  def translate_article
+    translator_service = GoogleApi::Translate.new
+    if translator_service.client==false
+      @errors += translator_service.errors
+      false
+    else
+    translator_service.call_translation(@article.body, @from_language, @to_language)
     end
   end
 
