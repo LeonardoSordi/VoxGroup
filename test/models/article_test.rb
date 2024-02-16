00734d7ad8eb1ebfb168d35765c65ac71ce08f9e
Leonardo-Sordi <115223267+me-sparo@users.noreply.github.com>
2024-02-16 16:32:34 +0100

    Update article_test.rb

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -10,40 +10,35 @@ class ArticleTest < ActiveSupport::TestCase
   end
 
   test "article duplicat gets created after article creation" do
-    assert_enqueued_jobs 1 do
       assert_difference 'Article.count', 2 do
         Article.create(title: "titolo articolo", body: "autore con chiave", status: "public", author_id: @author.id, language: "it")
       end
     end
-  end
 
 
   test "article doesn't get translated because already in english" do
-    assert_enqueued_jobs 1 do
       assert_difference 'Article.count', 1 do
         Article.create(title: "titolo articolo", body: "autore con chiave", status: "public", author_id: @author.id, language: "en")
       end
     end
-  end
+
 
 
   test "article duplicate is the translated version of the original" do
 
-    assert_enqueued_jobs 1 do
       @article=FactoryBot.create(:article, title: "titolo articolo", body: "autore con chiave", status: "public", author_id: @author.id, language: "it")
       translated_article = Article.last
       assert_equal translated_article.body, @translated_body
     end
-  end
+
 
 
   test "original article body does not get modified upon translation" do
     original_body = "autore con chiave"
-    assert_enqueued_jobs 1 do
       @article=FactoryBot.create(:article, title: "titolo articolo", body: original_body, status: "public", author_id: @author.id, language: "it")
       assert_equal @article.body, original_body
     end
-  end
+
 
 
 
