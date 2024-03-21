43c4ab0814beb804336b1e15d97b324197872003
Leonardo-Sordi <115223267+me-sparo@users.noreply.github.com>
2024-03-21 18:21:02 +0100

    token auth limit, articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,6 @@ class ArticlesController < ApplicationController
 
   before_action :set_article, only: %i[ show edit update destroy ]
   before_action :set_author, only: [:create, :update, :destroy]
-  skip_before_action :verify_authenticity_token
 
 
 
